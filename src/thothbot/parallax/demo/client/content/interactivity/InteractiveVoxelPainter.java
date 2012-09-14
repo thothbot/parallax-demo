8e444667d6f017a8f4c5f38d0dac5eb67b1ce14a
thothbot <thothbot@gmail.com>
2012-09-15 00:57:15 +0400

    Updated code in the example

--- src/thothbot/parallax/demo/client/content/interactivity/InteractiveVoxelPainter.java
+++ src/thothbot/parallax/demo/client/content/interactivity/InteractiveVoxelPainter.java
@@ -19,17 +19,47 @@
 
 package thothbot.parallax.demo.client.content.interactivity;
 
+import java.util.List;
+
+import thothbot.parallax.core.client.AnimationReadyEvent;
+import thothbot.parallax.core.client.context.Canvas3d;
+import thothbot.parallax.core.client.controls.TrackballControls.STATE;
 import thothbot.parallax.core.shared.cameras.PerspectiveCamera;
+import thothbot.parallax.core.shared.core.Color;
+import thothbot.parallax.core.shared.core.Projector;
+import thothbot.parallax.core.shared.core.Ray;
+import thothbot.parallax.core.shared.core.Vector3;
+import thothbot.parallax.core.shared.geometries.CubeGeometry;
+import thothbot.parallax.core.shared.geometries.PlaneGeometry;
+import thothbot.parallax.core.shared.lights.AmbientLight;
+import thothbot.parallax.core.shared.lights.DirectionalLight;
+import thothbot.parallax.core.shared.materials.Material.SHADING;
+import thothbot.parallax.core.shared.materials.MeshBasicMaterial;
+import thothbot.parallax.core.shared.materials.MeshLambertMaterial;
+import thothbot.parallax.core.shared.objects.Mesh;
+import thothbot.parallax.core.shared.utils.ImageUtils;
 import thothbot.parallax.demo.client.ContentWidget;
 import thothbot.parallax.demo.client.Demo;
 import thothbot.parallax.demo.client.DemoAnnotations.DemoSource;
+import thothbot.parallax.demo.client.content.geometries.GeometryHierarchy.DemoScene;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.RunAsyncCallback;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.event.dom.client.MouseMoveEvent;
+import com.google.gwt.event.dom.client.MouseMoveHandler;
+import com.google.gwt.event.dom.client.MouseUpEvent;
+import com.google.gwt.event.dom.client.MouseUpHandler;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.RootPanel;
 
-public final class InteractiveVoxelPainter extends ContentWidget 
+public final class InteractiveVoxelPainter extends ContentWidget implements  MouseMoveHandler, MouseDownHandler, KeyDownHandler, KeyUpHandler
 {
 	/*
 	 * Prepare Rendering Scene
@@ -37,25 +67,76 @@ public final class InteractiveVoxelPainter extends ContentWidget
 	@DemoSource
 	class DemoScene extends DemoAnimatedScene 
 	{
-
+	
+		private static final String texture = "./static/textures/square-outline-textured.png";
+		
+		Projector projector;
+		
+		Vector3 mouse2D;
+		
+		public boolean isShiftDown, isCtrlDown;
+		
 		@Override
 		protected void loadCamera()
 		{
 			setCamera(
 					new PerspectiveCamera(
-							70, // fov
+							45, // fov
 							getRenderer().getCanvas().getAspectRation(), // aspect 
 							1, // near
-							1000 // far 
+							10000 // far 
 					)); 
 		}
 
 		@Override
 		protected void onStart()
 		{
-			getCamera().getPosition().setZ(400);
+			getCamera().getPosition().setZ(800);
 			getScene().add(getCamera());
 
+			// roll-over helpers
+
+			CubeGeometry rollOverGeo = new CubeGeometry( 50, 50, 50 );
+			MeshBasicMaterial rollOverMaterial = new MeshBasicMaterial();
+			rollOverMaterial.setColor(new Color(0xff0000));
+			rollOverMaterial.setOpacity(0.5);
+			rollOverMaterial.setTransparent(true);
+			Mesh rollOverMesh = new Mesh( rollOverGeo, rollOverMaterial );
+			getScene().add( rollOverMesh );
+
+			// cubes
+
+			CubeGeometry cubeGeo = new CubeGeometry( 50, 50, 50 );
+			MeshLambertMaterial cubeMaterial = new MeshLambertMaterial();
+			cubeMaterial.setShading(SHADING.FLAT);
+			cubeMaterial.setMap(ImageUtils.loadTexture( texture ));
+			cubeMaterial.getColor().setHSV( 0.1, 0.7, 1.0 );
+			cubeMaterial.setAmbient(cubeMaterial.getColor());
+
+			// picking
+
+			projector = new Projector();
+
+			// grid
+
+			MeshBasicMaterial planeMaterial = new MeshBasicMaterial();
+			planeMaterial.setColor(new Color(0x555555));
+			planeMaterial.setWireframe(true);
+			Mesh plane = new Mesh( new PlaneGeometry( 1000, 1000, 20, 20 ), planeMaterial );
+			plane.getRotation().setX( - Math.PI / 2 );
+			getScene().add( plane );
+
+			mouse2D = new Vector3( 0, 10000, 0.5 );
+
+			// Lights
+
+			getScene().add( new AmbientLight( 0x606060 ) );
+
+			DirectionalLight directionalLight = new DirectionalLight( 0xffffff );
+			directionalLight.getPosition().set( 1, 0.75, 0.5 ).normalize();
+			getScene().add( directionalLight );
+
+			getRenderer().setClearColorHex(0xeeeeee);
 		}
 		
 		@Override
@@ -66,13 +147,125 @@ public final class InteractiveVoxelPainter extends ContentWidget
 		@Override
 		protected void onUpdate(double duration)
 		{
+			if ( isShiftDown ) 
+			{
+				theta += mouse2D.getX() * 3;
+			}
 
+			ray = projector.pickingRay( mouse2D.clone(), getCamera() );
+
+			List<Ray.Intersect> intersects = ray.intersectObjects( getScene().getChildren() );
+
+			if ( intersects.size() > 0 ) 
+			{
+
+				intersector = getRealIntersector( intersects );
+				if ( intersector ) 
+				{
+					setVoxelPosition( intersector );
+					rollOverMesh.position = voxelPosition;
+				}
+			}
+
+			getCamera().getPosition().setX( 1400 * Math.sin( theta * Math.PI / 360 ) );
+			getCamera().getPosition().setZ( 1400 * Math.cos( theta * Math.PI / 360 ) );
+
+			getCamera().lookAt( getScene().getPosition() );
 		}
 	}
 		
 	public InteractiveVoxelPainter() 
 	{
 		super("Voxel painter", "Add voxel: [click]; Remove voxel: [control + click]; Rotate: [shift + click]. This example based on the three.js example.");
+	}
+	
+	@Override
+	public void onAnimationReady(AnimationReadyEvent event)
+	{
+		super.onAnimationReady(event);
+  	
+	  	RootPanel.get().addDomHandler(this, KeyDownEvent.getType());
+		RootPanel.get().addDomHandler(this, KeyUpEvent.getType());
+		getWidget().addDomHandler(this, MouseMoveEvent.getType());
+		getWidget().addDomHandler(this, MouseDownEvent.getType());
+	}
+	
+	@Override
+	public void onMouseDown(MouseDownEvent event) 
+	{
+		event.preventDefault();
+
+		DemoScene rs = (DemoScene) renderingPanel.getAnimatedScene();
+		
+		var intersects = rs.ray.intersectObjects( scene.children );
+
+		if ( intersects.length > 0 ) 
+		{
+			intersector = getRealIntersector( intersects );
+
+			// delete cube
+			if ( rs.isCtrlDown ) 
+			{
+				if ( intersector.object != plane ) 
+				{
+					scene.remove( intersector.object );
+				}
+			}
+			// create cube
+			else 
+			{
+				intersector = getRealIntersector( intersects );
+				setVoxelPosition( intersector );
+
+				Mesh voxel = new Mesh( cubeGeo, cubeMaterial );
+				voxel.getPosition().copy( voxelPosition );
+				voxel.setMatrixAutoUpdate(false);
+				voxel.updateMatrix();
+				rs.getScene().add( voxel );
+			}
+		}
+	}
+
+	@Override
+	public void onMouseMove(MouseMoveEvent event) 
+	{
+		event.preventDefault();
+		
+		DemoScene rs = (DemoScene) renderingPanel.getAnimatedScene();
+	  	Canvas3d canvas = renderingPanel.getRenderer().getCanvas();
+
+	  	rs.mouse2D.setX( (event.getX() / canvas.getWidth() ) * 2 - 1 ); 
+	  	rs.mouse2D.setY( - (event.getY() / canvas.getHeight() ) * 2 + 1 );	
+	}
+
+	@Override
+	public void onKeyUp(KeyUpEvent event) 
+	{
+		DemoScene rs = (DemoScene) renderingPanel.getAnimatedScene();
+		
+		if ( event.getNativeEvent().getKeyCode() == 16 ) 
+		{
+			rs.isShiftDown = false;
+		} 
+		else if ( event.getNativeEvent().getKeyCode() == 17 ) 
+		{
+			rs.isCtrlDown = false;
+		} 
+	}
+
+	@Override
+	public void onKeyDown(KeyDownEvent event) 
+	{
+		DemoScene rs = (DemoScene) renderingPanel.getAnimatedScene();
+		
+		if ( event.getNativeEvent().getKeyCode() == 16 ) 
+		{
+			rs.isShiftDown = true;
+		} 
+		else if ( event.getNativeEvent().getKeyCode() == 17 ) 
+		{
+			rs.isCtrlDown = true;
+		} 
 	}
 	
 	@Override
@@ -103,5 +296,4 @@ public final class InteractiveVoxelPainter extends ContentWidget
 			}
 		});
 	}
-
 }
